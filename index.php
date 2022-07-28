0cc1b2565061391e3db84aac20bf7e28522d021c
Madhuri Dere <madhuridere2018@gmail.com>
2022-07-28 11:20:04 +0530

    c

--- index.php
+++ index.php
@@ -24,13 +24,39 @@ if(isset($_POST['submit1'])){
       <title></title>
       <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700" rel="stylesheet">
       <style>
-        
+          html,
+  body {
+      margin: 0 auto !important;
+      padding: 0 !important;
+      height: 100% !important;
+      width: 100% !important;
+      background: #f1f1f1;
+  }
+  * {
+      -ms-text-size-adjust: 100%;
+      -webkit-text-size-adjust: 100%;
+  }
+  
       </style>
   </head>
   <body width="100%" style="margin: 0; padding: 0 !important; mso-line-height-rule: exactly; background-color: #f1f1f1;">
-     <div>
-     <h1>'.$email.'</h1>
-     </div>
+  <div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
+  <div style="margin:50px auto;width:70%;padding:20px 0">
+    <div style="border-bottom:1px solid #eee">
+      <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Agreerent</a>
+    </div>
+    <p style="font-size:1.1em">Hi '.$name.'</p>
+    <p>Please enter below OTP to verify your Email id.</p>
+    <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">'.$otp.'</h2>
+    <p style="font-size:0.9em;">Regards,<br />Your Brand</p>
+    <hr style="border:none;border-top:1px solid #eee" />
+    <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
+      <p>Your Brand Inc</p>
+      <p>1600 Amphitheatre Parkway</p>
+      <p>California</p>
+    </div>
+  </div>
+  </div>
   </body>
   </html>';
   
