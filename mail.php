0e70b909251e794cdb0d90f426b09693988587e3
snehalambavale <snehal7039@gmail.com>
2022-08-05 15:10:00 +0530

    mail

--- mail.php
+++ mail.php
@@ -1,80 +1,79 @@
 <?php
+//clients POST & Email->Password
 
 if(isset($_POST['submit1'])){
     $status=1;
     $name=$_POST['name'];  
-    $email=$_POST['email'];    
+    $email=$_POST['email'];  
+    $mob_number=$_POST['mob_number'];  
     $select=$_POST['select'];
-  $from = 'Enquiry <'.$email.'>' . "\r\n";
-  $sendTo = 'Enquiry <snehal7039@gmail.com>';
-  $subject = 'Contact Form';
-  // $fields = array( 'name' => 'name' );
-  $from = 'Aayushi water purifier';
-  $from .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-  
-  
-  $emailText = '
-  <html>
-  <head>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="x-apple-disable-message-reformatting"> 
-      <title></title>
-      <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700" rel="stylesheet">
-      <style>
-          html,
-  body {
-      margin: 0 auto !important;
-      padding: 0 !important;
-      height: 100% !important;
-      width: 100% !important;
-      background: #f1f1f1;
+
+$from = 'Enquiry <yadavpriya1425@gmail.com>' . "\r\n";
+$sendTo = 'Enquiry <'.$email.'>';
+$subject = 'Contact form';
+// $fields = array( 'name' => 'name' );
+$from = 'Vaishali Enterprise' . "\r\n";
+$from .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+
+$emailText = '
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="x-apple-disable-message-reformatting"> 
+  <title></title>
+  <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700" rel="stylesheet">
+  <style>
+      html,
+body {
+  margin: 0 auto !important;
+  padding: 0 !important;
+  height: 100% !important;
+  width: 100% !important;
+  background: #f1f1f1;
+}
+* {
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+}
+  </style>
+</head>
+<body width="100%" style="margin: 0; padding: 0 !important; mso-line-height-rule: exactly; background-color: #f1f1f1;">
+<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
+<div style="margin:50px auto;width:70%;padding:20px 0">
+<div style="border-bottom:1px solid #eee">
+  <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">CRM</a>
+</div>
+<p style="font-size:1.1em">Hi '.$name.'</p>
+<p>Please enter below password</p>
+<h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">'.$email.'</h2>
+<p style="font-size:0.9em;">Regards,<br />Your Brand</p>
+<hr style="border:none;border-top:1px solid #eee" />
+</div>
+</div>
+</body>
+</html>';
+
+try{
+foreach($_POST as $key => $value){
+  if(isset($fields[$key])){
+    $emailText.="$fields[$key]: $value\n";
   }
-  * {
-      -ms-text-size-adjust: 100%;
-      -webkit-text-size-adjust: 100%;
-  }
-  
-      </style>
-  </head>
-  <body width="100%" style="margin: 0; padding: 0 !important; mso-line-height-rule: exactly; background-color: #f1f1f1;">
-  <div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
-  <div style="margin:50px auto;width:70%;padding:20px 0">
-    <div style="border-bottom:1px solid #eee">
-      <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Agreerent</a>
-    </div>
-    <p style="font-size:1.1em">Hi '.$name.'</p>
-    <p>Please enter below OTP to verify your Email id.</p>
-    <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">'.$select.'</h2>
-    <p style="font-size:0.9em;">Regards,<br />Your Brand</p>
-    <hr style="border:none;border-top:1px solid #eee" />
-    <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
-      <p>Your Brand Inc</p>
-      <p>1600 Amphitheatre Parkway</p>
-      <p>California</p>
-    </div>
-  </div>
-  </div>
-  </body>
-  </html>';
-  
-  try{
-    foreach($_POST as $key => $value){
-      if(isset($fields[$key])){
-        $emailText.="$fields[$key]: $value\n";
-      }
-    }
-   if( mail($sendTo,$subject,$emailText, "From:" .$from)){
-    echo "<script>alert('message send sucessfully!')</script>";
-   
-   }else{
-      echo "$sendTo $subject $emailText $from";
-   }
-  }
-  catch(\Exception $e){
-    echo "not done";
-  }
+}
+if( mail($sendTo,$subject,$emailText, "From:" .$from)){
+
+    echo"submit";
  
-  }
+}else{
+  echo "eeee $sendTo $subject $emailText $from";
+}
+}
+catch(\Exception $e){
+echo "not done";
+}
+
+
+}
 ?>
