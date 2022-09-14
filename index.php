d49090eeb5b125f3e34c213f4835c1eeaa5c26ff
snehalambavale <snehal7039@gmail.com>
2022-09-14 09:38:09 +0530

    c

--- index.php
+++ index.php
@@ -1464,7 +1464,7 @@ Thank you for being my best financial consultant.</p>
 
 
     <div class="mobile-nav__wrapper">
-        <div class="mobile-nav__overlay mobile-nav__toggler"></div>
+        <!-- <div class="mobile-nav__overlay mobile-nav__toggler"></div> -->
         <!-- /.mobile-nav__overlay -->
         <div class="mobile-nav__content">
             <span class="mobile-nav__close mobile-nav__toggler"><i class="fa fa-times"></i></span>
