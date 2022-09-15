db7f23ee3c0c1b771c73bde054a3a3a323b1eceb
aniketnangare23 <aniketnangare23@gmail.com>
2022-09-15 11:35:04 +0530

    map

--- index.php
+++ index.php
@@ -1131,12 +1131,12 @@ Thank you for being my best financial consultant.</p>
                     </div>
                 </div>
             </section>
-        <!-- <section class="google-map-two">
-            <iframe rel="preconnect" 
+        <section class="google-map-two">
+            <iframe defer rel="preconnect" 
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.2076432947365!2d73.00465641507097!3d19.09854458707495!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c18d23a43063%3A0xc40491427a3d4bef!2sVAISHALI%20ENTERPRISES!5e0!3m2!1sen!2sin!4v1659683597357!5m2!1sen!2sin" width="600" height="450"
                 class="google-map__two" allowfullscreen></iframe>
 
-        </section> -->
+        </section>
 
 
          <footer class="site-footer">
@@ -1209,9 +1209,9 @@ Thank you for being my best financial consultant.</p>
                 <div class="site-footer__bottom">
                     <div class="row">
                         <div class="col-xl-12">
-                            <div class="site-footer__bottom-inner">
+                             <div class="site-footer__bottom-inner">
                                 <p class="site-footer__bottom-text" style="float:left;">© All Copyright 2022 by <a href="#">Vaishali Enterprises</a>
-                                </p> <p class="site-footer__bottom-text" style="float:right;">Designed By <a href="https://tectignis.in" target="_blank"><b>Tectignis IT Solutions.</b></a></p>
+                                </p> <p class="site-footer__bottom-text" style="text-align:end">Designed By <a href="https://tectignis.in" target="_blank"><b>Tectignis IT Solutions.</b></a></p>
                             </div>
                         </div>
                     </div>
